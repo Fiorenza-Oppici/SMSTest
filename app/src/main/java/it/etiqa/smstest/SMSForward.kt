2233b6b186506f82b44b904018006a2c337d4758
Fiorenza Oppici <fiorenza.oppici@hoverstate.com>
2018-09-21 10:49:30 +0200

    send http request over async thread - GET

--- app/src/main/java/it/etiqa/smstest/SMSForward.kt
+++ app/src/main/java/it/etiqa/smstest/SMSForward.kt
@@ -2,13 +2,14 @@ package it.etiqa.smstest
 
 import android.os.AsyncTask
 import android.util.Log
-import khttp.get
+import com.github.kittinunf.fuel.httpGet
 
 class SMSForward : AsyncTask<String, String, String>() {
     val TAG = "SMSActivity"
 
     override fun doInBackground(vararg params: String): String {
-        Log.i(TAG, "ciaoo")
-        return get(params?.get(0)).text
+        val (request, response, result) = params?.get(0).httpGet().responseString()
+        Log.i(TAG, response.responseMessage)
+        return response.toString()
     }
 }
