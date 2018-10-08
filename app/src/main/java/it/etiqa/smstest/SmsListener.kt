f7f79c36e7557026367bd6736a01c7f5429ae8ca
Fiorenza Oppici <fiorenza.oppici@hoverstate.com>
2018-10-08 15:50:16 +0200

    implement filter in broadcastreceiver

--- app/src/main/java/it/etiqa/smstest/SmsListener.kt
+++ app/src/main/java/it/etiqa/smstest/SmsListener.kt
@@ -16,17 +16,32 @@ class SmsListener : BroadcastReceiver() {
 
         val recording = sharedPref.getBoolean(context.getString(R.string.activated_state_label), false)
         val targetUrl = sharedPref.getString(context.getString(R.string.server_url_label), context.resources.getString(R.string.server_url_placeholder))
+        val filterTags = sharedPref.getStringSet("filter_tags", HashSet<String>()).toHashSet()
 
         Log.i(TAG,"Sms received!________________________________________")
         if (recording) {
             if (Telephony.Sms.Intents.SMS_RECEIVED_ACTION.equals(intent.getAction())) {
                 for (smsMessage: SmsMessage in Telephony.Sms.Intents.getMessagesFromIntent(intent)) {
                     val messageBody = smsMessage.messageBody
-                    SMSForward().execute(targetUrl, messageBody)
+                    if (checkForTag(messageBody, filterTags)) {
+                        SMSForward().execute(targetUrl, messageBody)
+                    } else {
+                        Log.i(TAG, "message doesn't contain any of the tags")
+                    }
                 }
             }
         } else {
             Log.i(TAG, "Message received - but transferral has not been enabled!")
         }
+    }
+
+    private fun checkForTag(message: String, tagsList: HashSet<String>) :  Boolean {
+        val containsTag = false
+        for (str in tagsList) {
+            if (message.contains(str.toLowerCase())) {
+                return true
+            }
+        }
+        return containsTag
     }
 }
